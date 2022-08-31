b74bcde594c175a9aa6e49a0eb7940897c3eb016
Marius <52743525+avolgha@users.noreply.github.com>
2022-08-31 10:40:52 +0000

    removed third_party libraries and ability to flat array

--- src/object.ts
+++ src/object.ts
@@ -1,3 +1,5 @@
+import { flatten } from "flat";
+
 /**
  * Create an object with default options and pass the other object on it.
  *
@@ -12,4 +14,31 @@ export function obj_default<A, B>(object: A, defaultObject: B): A & B {
     };
 }
 
-export function obj_flat(object: any): any {}
+/**
+ * Flat an object.
+ * 
+ * @param object The object you want to flat.
+ * @returns The flatten object.
+ * 
+ * @example
+ * obj_flat({ x: { y: 2 }, z: 4 });
+ * // => { "x.y": 2, z: 4 }
+ */
+export function obj_flat(object: any): any {
+    return flatten(object);
+}
+
+/**
+ * Get a key from an object.
+ * 
+ * @param object The object you want to get the value from.
+ * @param key The key of the value.
+ * @returns The value you get through the key and the object.
+ * 
+ * @example
+ * obj_key({ x: { y: 2 }, z: 4 }, "x.y");
+ * // => 2
+ */
+export function obj_key(object: any, key: string): any {
+    return obj_flat(object)[key];
+}
