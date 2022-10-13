7e76a809fcd65fee1d159d4c3c3fe5fdcbd50131
Marius <52743525+avolgha@users.noreply.github.com>
2022-10-13 17:26:24 +0000

    noticed a little bug in type checking

--- src/object.ts
+++ src/object.ts
@@ -7,7 +7,7 @@ import { flatten } from "./internal/flat";
  * obj_default({ a: 2, b: 1 }, { a: 1, c: 3 });
  * // => { a: 2, b: 1, c: 3 }
  */
-export function obj_default<A, B>(object: A, defaultObject: B): A & B {
+export function obj_default<A, B = A>(object: A, defaultObject: B): A & B {
     return {
         ...defaultObject,
         ...object,
